bb7d81b3aadaa8de80210f60ba8edfaabe89ddd3
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-07 22:37:17 +0100

    fix: js -> mjs

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsdown'
 const config: ReturnType<typeof defineConfig> = defineConfig({
   outDir: 'dist',
   entry: 'src/cli.ts',
+  fixedExtension: true,
   dts: true,
   clean: true,
   bundleDts: true
