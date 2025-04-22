78b56aa0f3d3c315801913c33c6e5040e0b4d652
ryoppippi <1560508+ryoppippi@users.noreply.github.com>
2025-04-22 12:11:44 +0100

    feat!: migrate from cac to cleye

--- src/cli.ts
+++ src/cli.ts
@@ -1,44 +1,62 @@
-import cac from "cac";
+import process from "node:process";
+import { cli } from "cleye";
 import { version } from "../package.json";
 import { startServer } from "./server.ts";
-import { type StartServerOptions, TOOL_NAME_STRATEGIES } from "./types.ts";
+import { TOOL_NAME_STRATEGIES, type ToolNameStrategy } from "./types.ts";
 
-const cli = cac("sitemcp");
-
-cli
-	.command("[url]", "Fetch a site")
-	.option("--concurrency <number>", "Number of concurrent requests", {
-		default: 3,
-	})
-	.option("-m, --match <pattern>", "Only fetch matched pages")
-	.option("--content-selector <selector>", "The CSS selector to find content")
-	.option("--limit <limit>", "Limit the result to this amount of pages")
-	.option("--no-cache", "Do not use cache")
-	.option(
-		"-t, --tool-name-strategy <strategy>",
-		`Tool name strategy (${TOOL_NAME_STRATEGIES.join(", ")})`,
-		{
-			default: "domain",
+const argv = cli({
+	name: "sitemcp",
+	version,
+	parameters: [
+		"<url>", // The URL to fetch
+	],
+	flags: {
+		concurrency: {
+			type: Number,
+			default: 3,
+			alias: "c",
+			description: "Number of concurrent requests",
 		},
-	)
-	.option(
-		"-l, --max-length <number>",
-		"Maximum length of the content to return",
-		{ default: 2000 },
-	)
-	.action(async (url: string, flags: StartServerOptions) => {
-		if (!url) {
-			cli.outputHelp();
-			return;
-		}
+		match: {
+			type: [String],
+			alias: "m",
+			description: "Only fetch matched pages",
+		},
+		contentSelector: {
+			type: String,
+			description: "The CSS selector to find content",
+		},
+		limit: {
+			type: Number,
+			description: "Limit the result to this amount of pages",
+		},
+		noCache: {
+			type: Boolean,
+			default: false,
+			description: "Use cache",
+		},
+		toolNameStrategy: {
+			type: (type: ToolNameStrategy): ToolNameStrategy => {
+				if (!TOOL_NAME_STRATEGIES.includes(type)) {
+					throw new Error(`Invalid tool name strategy: ${type}`);
+				}
+				return type;
+			},
+			default: "domain" as const,
+			alias: "t",
+			description: `Tool name strategy (${TOOL_NAME_STRATEGIES.join(", ")})`,
+		},
+		maxLength: {
+			type: Number,
+			default: 2000,
+			alias: "l",
+			description: "Maximum length of the content to return",
+		},
+	},
+});
 
-		await startServer({
-			...flags,
-			url,
-			cache: flags.cache ?? true, // we can use default option of cac, but science we use `--no` prefix, we don't want to show the user that the default is true. It is misleading.
-		});
-	});
-
-cli.version(version);
-cli.help();
-cli.parse();
+await startServer({
+	...argv.flags,
+	cache: !argv.flags.noCache,
+	url: argv._.url,
+});
